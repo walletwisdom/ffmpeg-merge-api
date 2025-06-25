08459be5c71dff19c2ba8ccf48a141e972184d19
walletwisdom <walletwisdomnow@gmail.com>
2025-06-24 20:31:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/merge", async (req, res) => {
         fs.unlinkSync(videoPath);
         fs.unlinkSync(audioPath);
 
-        const fileUrl = `${req.protocol}://${req.get("host")}/public/${outputFilename}`;
+        const fileUrl = `${req.protocol}://${req.get("host")}/${outputFilename}`;
         res.json({ fileUrl });
       })
       .on("error", (err) => {
